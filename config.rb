3180353c19c5e332c7dfec1519a9f77a1023db02
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-02-17 01:44:42 +0100

    some update on exercices at Le Wagon and don't have a good discipline yet on commit...

--- config.rb
+++ config.rb
@@ -30,7 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
+data.flats.each do |member, flat_data|
+  proxy "/flats/#{owner}.html", "/flats/show.html", locals: {owner: member, flat: flat_data}
+end
 ###
 # Helpers
 ###
